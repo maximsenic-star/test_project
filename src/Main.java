b1b7a3124fdac40f4cdc280b985c80197df2a26d
Maxim Senic <maxim.senic@gmail.com>
2026-02-05 12:38:01 +0100

    add all new

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         int i = 5;
-        System.out.println("Hello Test Project");
+        System.out.println("Hello Test Project" + i);
     }
 }
 
